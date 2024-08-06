c053e097225c7c74cdd5b8a3b736002b9118e81b
KadirovKmck <kadirovmuhammadaziz79@gmail.com>
2024-08-06 13:42:38 +0600

    kz

--- lib/main.dart
+++ lib/main.dart
@@ -1,120 +1,123 @@
+import 'package:add_to_google_wallet/widgets/add_to_google_wallet_button.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:uuid/uuid.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MaterialApp(home: MyApp()));
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: LoyaltyProgramScreen(),
-    );
-  }
-}
-
-class LoyaltyProgramScreen extends StatefulWidget {
-  const LoyaltyProgramScreen({super.key});
-
-  @override
-  State<LoyaltyProgramScreen> createState() => _LoyaltyProgramScreenState();
-}
-
-class _LoyaltyProgramScreenState extends State<LoyaltyProgramScreen> {
-  final _formKey = GlobalKey<FormState>();
-  String _username = '';
-  String _cardId = '';
-
-  Future<void> _registerLoyaltyCard() async {
-    if (_formKey.currentState!.validate()) {
-      _formKey.currentState!.save();
-
-      final response = await http.post(
-        Uri.parse('YOUR_BACKEND_ENDPOINT/createLoyaltyCard'),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: json.encode({'username': _username}),
-      );
-
-      if (response.statusCode == 200) {
-        final responseData = json.decode(response.body);
-        setState(() {
-          _cardId = responseData['cardId'];
-        });
-        _addCardToGoogleWallet(_cardId);
-      } else {
-        // Handle error
-        print('Failed to create loyalty card');
-      }
-    }
-  }
-
-  Future<void> _addCardToGoogleWallet(String cardId) async {
-    final response = await http.post(
-      Uri.parse(
-          'https://walletobjects.googleapis.com/walletobjects/v1/loyaltyObject'),
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer YOUR_ACCESS_TOKEN',
-      },
-      body: json.encode({
-        "id": "loyaltyObjectId",
-        "classId": "YOUR_CLASS_ID",
-        "state": "active",
-        "accountId": cardId,
-        "accountName": _username,
-        // Add other required fields for the loyalty card
-      }),
-    );
-
-    if (response.statusCode == 200) {
-      // Successfully added to Google Wallet
-      print('Card added to Google Wallet');
-    } else {
-      // Handle error
-      print('Failed to add card to Google Wallet');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Loyalty Program'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Username'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your username';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _username = value!;
-                },
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _registerLoyaltyCard,
-                child: const Text('Register Loyalty Card'),
-              ),
-              if (_cardId.isNotEmpty) Text('Card ID: $_cardId'),
-            ],
+  Widget build(BuildContext context) => Scaffold(
+        body: Center(
+          child: AddToGoogleWalletButton(
+            pass: _examplePass,
+            onSuccess: () => _showSnackBar(context, 'Success!'),
+            onCanceled: () => _showSnackBar(context, 'Action canceled.'),
+            onError: (Object error) => _showSnackBar(context, error.toString()),
+            locale: const Locale.fromSubtags(
+              languageCode: 'en',
+              countryCode: 'US', // Corrected country code
+            ),
           ),
         ),
-      ),
-    );
-  }
+      );
+
+  void _showSnackBar(BuildContext context, String text) =>
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(text)));
 }
+
+final String _passId = const Uuid().v4();
+const String _passClass = 'coffestore';
+const String _issuerId = '3388000000022289137';
+const String _issuerEmail =
+    'kadirovmuhammadaziz79@gmail.com'; // Consider using a placeholder
+
+final String _examplePass = """
+    {
+      "iss": "$_issuerEmail",
+      "aud": "google",
+      "typ": "savetowallet",
+      "origins": [],
+      "payload": {
+        "genericObjects": [
+          {
+            "id": "$_issuerId.$_passId",
+            "classId": "$_issuerId.$_passClass",
+            "genericType": "GENERIC_TYPE_UNSPECIFIED",
+            "hexBackgroundColor": "#ffffff",
+            "logo": {
+              "sourceUri": {
+                "uri": "https://github.com/KadirovAd/google_wallet/blob/main/assets/logo.png?raw=true"
+              }
+            },
+            "cardTitle": {
+              "defaultValue": {
+                "language": "en",
+                "value": "Coffe store"
+              }
+            },
+            "subheader": {
+              "defaultValue": {
+                "language": "en",
+                "value": "Attdddddendee"
+              }
+            },
+            "header": {
+              "defaultValue": {
+                "language": "en",
+                "value": "Alex dddddd McJacobs"
+              }
+            },
+            "barcode": {
+              "type": "QR_CODE",
+              "value": "$_passId"
+            },
+            "heroImage": {
+              "sourceUri": {
+                "uri": "https://i.pinimg.com/564x/e5/53/49/e553499143a89278217792a4d1147047.jpg"
+              }
+            },
+            "textModulesData": [
+              {
+                "header": "POINTS",
+                "body": "1234",
+                "id": "points"
+              }
+            ]
+          }
+        ]
+      }
+    }
+""";
+// const String _examplePass = """
+//     {
+//       "iss": "$_issuerEmail",
+//       "aud": "google",
+//       "typ": "savetowallet",
+//       "origins": [],
+//       "payload": {
+//       "loyaltyObjects": [
+//           {
+//           "barcode": {
+//             "alternateText": "12345",
+//             "type": "qrCode",
+//             "value": "28343E3"
+//           },
+//         "id": "123456789_LoyaltyObject",
+//           "loyaltyPoints": {
+//             "balance": {
+//               "string": "500"
+//             },
+//           "label": "Points"
+//         },
+//           "accountId": "1234567890",
+//           "classId": "123456789.LoyaltyClass",
+//           "accountName": "Jane Doe",
+//           "state": "active",
+//           "version": 1
+//           }
+//         ]
+//       }
+//     }
+// """;
